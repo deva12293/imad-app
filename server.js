243dd4a74cf3057ded54a85f46bb160ea3131ab6
deva12293 <31220865+deva12293@users.noreply.github.com>
2017-08-30 00:24:41 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
-});
 app.get('/EDUCATION', function (req,res) {
 res.send(`12th pass..Electronics Engineer.. `);
 });
+
 app.get('/AIM', function (req,res) {
-res.send('To earn money..and to do something good for this society');
+res.sendFile(path.join(__dirname, 'ui', 'AIM.html'));
 });
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
