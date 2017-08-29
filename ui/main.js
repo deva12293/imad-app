d1707075a24aca5005a9aa48abdf0ccb6b8305fc
deva12293 <31220865+deva12293@users.noreply.github.com>
2017-08-30 05:22:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,10 +6,10 @@ var img= document.getElementById('deva');
 var marginLeft = 0;
 
 function moveRight () {
-    marginLeft= marginLeft + 5;
+    marginLeft= marginLeft + 1;
     img.style.marginLeft= marginLeft + 'px';
 }
 
 img.onclick = function () {
-var interval = setInterval(moveRight, 100);
+var interval = setInterval(moveRight, 5);
 };
