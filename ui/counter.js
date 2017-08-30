e3271954c16e0ac86e988e725ffb95f0ef471bcd
deva12293 <31220865+deva12293@users.noreply.github.com>
2017-08-30 06:14:46 +0530

    [imad-console] Updates ui/counter.js

--- ui/counter.js
+++ ui/counter.js
@@ -1,1 +1,10 @@
+var button = document.getElementById('RefreshCounter');
+var RefreshCounter = 0;
+button.onclick = function() {
+    RefreshCounter= RefreshCounter + 1;
+    var span= document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+};
+
 
