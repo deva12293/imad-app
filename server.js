cd205957059531f27ce4923abaeca4e32fcba6cc
deva12293 <31220865+deva12293@users.noreply.github.com>
2017-08-30 05:52:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var RefreshCounter= 0;
+app.get('/RefreshCounter', function (req, res) {
+RefreshCounter= RefreshCounter +1;
+res.send(RefreshCounter.tostring());
+});
+
 app.get('/education', function (req,res) {
 res.sendFile(path.join(__dirname, 'ui', 'education.html'));
 });
