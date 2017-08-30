0d2c4ac6cd57ee00c788112737f9622bcf835c1f
deva12293 <31220865+deva12293@users.noreply.github.com>
2017-08-30 06:22:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -14,5 +14,14 @@ img.onclick = function () {
 var interval = setInterval(moveRight, 1);
 };
 
+var button = document.getElementById('RefreshCounter');
+var RefreshCounter = 0;
+button.onclick = function() {
+    RefreshCounter= RefreshCounter + 1;
+    var span= document.getElementById('count');
+    span.innerHTML = counter.toString();
+    
+};
+
 
 
